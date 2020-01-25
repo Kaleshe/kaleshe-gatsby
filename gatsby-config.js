7fe91af359de12413c81a20a7804fdd845485c61
Kaleshe Alleyne-Vassel <kalesheav@gmail.com>
2020-01-25 21:14:32 +0000

    'init'

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+module.exports = {
+   siteMetadata: {
+      title: 'Kaleshe',
+      siteUrl: 'https://kaleshe.co.uk',
+      description: 'Digital Designer & Web Developer based in London'
+   }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+
+export default () => (
+   <Link to="/contact/">Contact</Link>
+)
